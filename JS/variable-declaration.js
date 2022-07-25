b1b5f1fb276be0b2f28876753b3a5e7d5896d673
devebe <dvbirdja@gmail.com>
2022-07-25 21:40:40 +0200

    Modify JS files to contain code examples

---
+++ JS/variable-declaration.js
@@ -0,0 +1,8 @@
+let message;
+// Declaring variable message that will store information
+
+message = "Hello world";
+// Store the string "Hello World" in variable named message
+
+alert (message);
+// Show the content that is stored in variable message
