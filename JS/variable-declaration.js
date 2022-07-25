73d59ae6d66de31eb66734046b31f03bb98cfec8
devebe <dvbirdja@gmail.com>
2022-07-25 21:41:34 +0200

    Modify index.html to show latest notes

--- JS/variable-declaration.js
+++ JS/variable-declaration.js
@@ -1,8 +1,37 @@
 let message;
 // Declaring variable message that will store information
 
-message = "Hello world";
+message = "Hello";
 // Store the string "Hello World" in variable named message
 
-alert (message);
+console.log(message);
 // Show the content that is stored in variable message
+
+let firstname = "John";
+// Declaring a variable message and immediately storing information
+// in it is possible too.
+
+let user;
+
+user = firstname;
+// Copies data from one variable into another
+
+console.log(message+" "+user);
+// User will show the same as firstname
+
+// When changing the stored information in a variable, one should not
+// use let twice as it will run into an error.
+
+// Variable naming rules
+
+// - The name must contain only letters, digits, or the symbols $ and _.
+// - The first character must not be a digit.
+// - Variables are case-sensitive so APPLE and apple are not the same
+// - Words such as let, class, return and function are reserved
+//   and cannot be used.
+
+// For longer names camelCasing is used
+
+
+
+
