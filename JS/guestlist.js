95dc8ccf398e80ca66b5c6cc9617ee951543aa45
devebe <dvbirdja@gmail.com>
2022-08-08 20:19:26 +0200

    Loop through people to filter them

--- JS/guestlist.js
+++ JS/guestlist.js
@@ -1,10 +1,19 @@
-// loop to iterate through people array
-// check if people equals 'Phil' or 'Lola'
-// if true concatenate to refused text paragraph
-// if false concatenate to admitted text paragraph
-
 const people = ['Chris','Anne','Colin','Terri','Phil','Lola','Sam','Kay','Bruce'];
 const admitted = document.querySelector('.admitted');
 const refused = document.querySelector('.refused')
 admitted.textContent = 'Admit: ';
 refused.textContent = 'Refused: ';
+
+// loop to iterate through people array
+for (let i = 0; i < people.length; i++) {
+    // check if people equals 'Phil' or 'Lola'
+    if (people[i] === 'Phil' || people[i] === 'Lola') {  
+        refused.textContent += `${people[i]}, `;
+    }
+    else {
+        admitted.textContent += `${people[i]}, `;
+    }
+}
+// remove last comma from string
+admitted.textContent = admitted.textContent.substring(0, (admitted.textContent.length - 2));
+refused.textContent = refused.textContent.substring(0, (refused.textContent.length - 2));
